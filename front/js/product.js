09a3f1dc3560a222618d123a3787b2d961933bb8
CyberGoalie <99082706+cybergoalie@users.noreply.github.com>
2023-05-11 13:54:46 -0700

    Continued product.js to add items to cart + jsdoc

--- front/js/product.js
+++ front/js/product.js
@@ -1,11 +1,32 @@
-const params = new URL(document.location).searchParams
-const id = params.get("id")
+/**
+ * Represents the parameters of the URL.
+ * @type {URLSearchParams}
+ */
+const params = new URL(document.location).searchParams;
 
-const url = `http://localhost:3000/api/products/${id}`
+/**
+ * The ID of the product.
+ * @type {string}
+ */
+const id = params.get("id");
 
-const container = document.getElementById("items")
+/**
+ * The URL of the product API endpoint.
+ * @type {string}
+ */
+const url = `http://localhost:3000/api/products/${id}`;
 
+/**
+ * The container element for the product details.
+ * @type {HTMLElement}
+ */
+const container = document.getElementById("items");
 
+/**
+ * Fetches the product with the specified ID and updates the product page with the details.
+ * @function getArticle
+ * @returns {void}
+ */
 const getArticle = () => {
     fetch(url)
     .then(function (res) {
@@ -19,7 +40,6 @@ const getArticle = () => {
         document.querySelector(".item__img").appendChild(addImg)
         addImg.setAttribute("src", `${data.imageUrl}`)
 
-
         const addDescription = (document.getElementById("description").innerHTML = data.description)
         const addOption = document.getElementById("colors")
         for (color in data.colors) {
@@ -28,4 +48,37 @@ const getArticle = () => {
     })
 }
 
-getArticle()
+/**
+ * The "Add to Cart" button element.
+ * @type {HTMLElement}
+ */
+const addToCart = document.getElementById("addToCart");
+
+addToCart.addEventListener("click", () => {
+    /**
+     * The product to be added to the shopping cart.
+     * @type {{quantity: string, color: string, id: string}}
+     */
+    const addProduct = {
+        quantity: document.getElementById("quantity").value,
+        color: document.getElementById("colors").value,
+        id: id
+    };
+
+    /**
+     * The array of products in the shopping cart.
+     * @type {Array<{quantity: string, color: string, id: string}>}
+     */
+    let addProductLocalStorage = [];
+
+    if (localStorage.getItem("addToCart") !== null) {
+        addProductLocalStorage = JSON.parse(localStorage.getItem("addToCart"));
+        addProductLocalStorage.push(addProduct);
+        localStorage.setItem("addToCart", JSON.stringify(addProductLocalStorage));
+    } else {
+        addProductLocalStorage.push(addProduct);
+        localStorage.setItem("addToCart", JSON.stringify(addProductLocalStorage));
+    }
+});
+
+getArticle();
