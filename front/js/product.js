0389fde5f106413f1ae2ed257b61dd6634176f84
CyberGoalie <99082706+cybergoalie@users.noreply.github.com>
2023-05-11 23:43:06 -0700

    cart.js & confirmation.js but why no confirmation?

--- front/js/product.js
+++ front/js/product.js
@@ -1,26 +1,33 @@
 /**
- * Represents the parameters of the URL.
+ * URLSearchParams interface provides utility methods to work with the query string of a URL.
+ * @external URLSearchParams
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams}
+ */
+
+/**
+ * The parameters object that represents the query string of the current URL.
  * @type {URLSearchParams}
  */
 const params = new URL(document.location).searchParams;
 
 /**
- * The ID of the product.
- * @type {string}
+ * The ID of the product to fetch from the API.
+ * @type {string|null}
  */
 const id = params.get("id");
 
 /**
- * The URL of the product API endpoint.
+ * The URL to fetch the product data from.
  * @type {string}
  */
 const url = `http://localhost:3000/api/products/${id}`;
 
 /**
- * The container element for the product details.
+ * The DOM element that will contain the product information.
  * @type {HTMLElement}
  */
 const container = document.getElementById("items");
+
 
 /**
  * Fetches the product with the specified ID and updates the product page with the details.
@@ -40,6 +47,7 @@ const getArticle = () => {
         document.querySelector(".item__img").appendChild(addImg)
         addImg.setAttribute("src", `${data.imageUrl}`)
 
+
         const addDescription = (document.getElementById("description").innerHTML = data.description)
         const addOption = document.getElementById("colors")
         for (color in data.colors) {
@@ -48,37 +56,23 @@ const getArticle = () => {
     })
 }
 
-/**
- * The "Add to Cart" button element.
- * @type {HTMLElement}
- */
 const addToCart = document.getElementById("addToCart");
-
 addToCart.addEventListener("click", () => {
-    /**
-     * The product to be added to the shopping cart.
-     * @type {{quantity: string, color: string, id: string}}
-     */
-    const addProduct = {
-        quantity: document.getElementById("quantity").value,
-        color: document.getElementById("colors").value,
-        id: id
-    };
+  const addProduct = {
+    quantity: document.getElementById("quantity").value,
+    color: document.getElementById("colors").value,
+    id: id
+  };
 
-    /**
-     * The array of products in the shopping cart.
-     * @type {Array<{quantity: string, color: string, id: string}>}
-     */
-    let addProductLocalStorage = [];
-
-    if (localStorage.getItem("addToCart") !== null) {
-        addProductLocalStorage = JSON.parse(localStorage.getItem("addToCart"));
-        addProductLocalStorage.push(addProduct);
-        localStorage.setItem("addToCart", JSON.stringify(addProductLocalStorage));
-    } else {
-        addProductLocalStorage.push(addProduct);
-        localStorage.setItem("addToCart", JSON.stringify(addProductLocalStorage));
-    }
+  let addProductLocalStorage = [];
+  if (localStorage.getItem("addToCart") !== null) {
+    addProductLocalStorage = JSON.parse(localStorage.getItem("addToCart"));
+    addProductLocalStorage.push(addProduct);
+    localStorage.setItem("addToCart", JSON.stringify(addProductLocalStorage));
+  } else {
+    addProductLocalStorage.push(addProduct);
+    localStorage.setItem("addToCart", JSON.stringify(addProductLocalStorage));
+  }
 });
 
 getArticle();
